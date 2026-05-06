d86fc0fd2da612dc92f55f317ea919adcdc6440a
gopal awasthi <122097808+gopalawasthi26@users.noreply.github.com>
2026-05-06 12:18:08 +0530

    [AC] 112A - 112A [Java 21 64bit]

---
+++ Codeforces/112/112A.java
@@ -0,0 +1,32 @@
+/*
+ * Problem  : 112A
+ * Contest  : 112 | Problem : A
+ * Language : Java 21 64bit
+ * Date     : 06 May 2026
+ */
+
+import java.util.Scanner;
+
+public class BeautifulMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int row = 0;
+        int col = 0;
+        
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (sc.nextInt() == 1) {
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+        
+        int moves = Math.abs(row - 3) + Math.abs(col - 3);
+        
+        System.out.println(moves);
+        sc.close();
+    }
+}
