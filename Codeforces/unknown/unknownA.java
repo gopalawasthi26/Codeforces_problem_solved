3a16c4ee153b4dcb056d4a6fca1152539a379b72
gopal awasthi <122097808+gopalawasthi26@users.noreply.github.com>
2026-05-23 09:49:55 +0530

    [AC] unknownA - unknownA [Java 21 64bit]

--- Codeforces/unknown/unknownA.java
+++ Codeforces/unknown/unknownA.java
@@ -2,32 +2,27 @@
  * Problem  : unknownA
  * Contest  : unknown | Problem : A
  * Language : Java 21 64bit
- * Date     : 12 May 2026
+ * Date     : 23 May 2026
  */
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Use Scanner for simple input reading
-        try (Scanner sc = new Scanner(System.in)) {
-            // Read initial number n and the number of subtractions k
-            int n = sc.nextInt();
-            int k = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
 
-            // Perform the "wrong subtraction" k times
-            for (int i = 0; i < k; i++) {
-                if (n % 10 == 0) {
-                    // Rule 1: If the last digit is 0, divide by 10
-                    n /= 10;
-                } else {
-                    // Rule 2: If the last digit is non-zero, subtract 1
-                    n -= 1;
-                }
+        for (int i = 0; i < t; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+
+            if (a + b == c || a + c == b || b + c == a) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
             }
-
-            
-            System.out.println(n);
         }
+        scanner.close();
     }
 }
