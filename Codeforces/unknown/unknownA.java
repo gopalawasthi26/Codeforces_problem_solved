e6095bfd0b6983ba4949a964cf0ff703cf429ea2
gopal awasthi <122097808+gopalawasthi26@users.noreply.github.com>
2026-05-30 07:10:27 +0530

    [AC] unknownA - unknownA [Java 21 64bit]

--- Codeforces/unknown/unknownA.java
+++ Codeforces/unknown/unknownA.java
@@ -2,25 +2,40 @@
  * Problem  : unknownA
  * Contest  : unknown | Problem : A
  * Language : Java 21 64bit
- * Date     : 29 May 2026
+ * Date     : 30 May 2026
  */
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s1 = sc.next().toLowerCase();
-        String s2 = sc.next().toLowerCase();
-        
-        int result = s1.compareTo(s2);
-        
-        if (result < 0) {
-            System.out.println(-1);
-        } else if (result > 0) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] counts = new int[5];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            counts[Integer.parseInt(st.nextToken())]++;
         }
+
+        int taxis = counts[4];
+        
+        taxis += counts[3];
+        counts[1] = Math.max(0, counts[1] - counts[3]);
+        
+        taxis += counts[2] / 2;
+        counts[2] %= 2;
+        
+        if (counts[2] > 0) {
+            taxis++;
+            counts[1] = Math.max(0, counts[1] - 2);
+        }
+        
+        taxis += (counts[1] + 3) / 4;
+
+        System.out.println(taxis);
     }
 }
