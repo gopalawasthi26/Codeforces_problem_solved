6539e70a1226bb1d594c6067144aaf25dff397c3
gopal awasthi <122097808+gopalawasthi26@users.noreply.github.com>
2026-06-02 10:45:59 +0530

    [AC] 630A - 630A [Java 21 64bit]

---
+++ Codeforces/630/630A.java
@@ -0,0 +1,20 @@
+/*
+ * Problem  : 630A
+ * Contest  : 630 | Problem : A
+ * Language : Java 21 64bit
+ * Date     : 02 Jun 2026
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNext()) {
+            scanner.next();
+        }
+        System.out.println(25);
+        scanner.close();
+    }
+}
+
