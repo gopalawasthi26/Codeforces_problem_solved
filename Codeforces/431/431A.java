f522e838e4a5141c05e5b3c991bd789d226f6878
gopal awasthi <122097808+gopalawasthi26@users.noreply.github.com>
2026-06-04 10:24:27 +0530

    [AC] 431A - 431A [Java 21 64bit]

---
+++ Codeforces/431/431A.java
@@ -0,0 +1,33 @@
+/*
+ * Problem  : 431A
+ * Contest  : 431 | Problem : A
+ * Language : Java 21 64bit
+ * Date     : 04 Jun 2026
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] cost = new int[4];
+        for (int i = 0; i < 4; i++) {
+            cost[i] = Integer.parseInt(st.nextToken());
+        }
+
+        String s = br.readLine();
+        long totalCalories = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int strip = s.charAt(i) - '1';
+            totalCalories += cost[strip];
+        }
+
+        System.out.println(totalCalories);
+    }
+}
